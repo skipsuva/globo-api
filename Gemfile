d2d054908870c23da74dff77514985d45fe74e87
skipsuva <skip.suva@gmail.com>
2016-02-19 14:19:44 -0500

    add auth token migration

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem 'dotenv-rails'
 gem 'rack-cors', :require => 'rack/cors'
 
 # To use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :trips, dependent: :destroy
   has_many :pins, dependent: :destroy
+
+  has_secure_password
+  before_save :verify_authentication_token
+
+
+  def verify_authentication_token
+    if authentication_token.empty?
+      self.authentication_token = generate_auth_token
+    end
+  end
 end
