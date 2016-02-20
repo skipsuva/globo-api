c0c7a78fbda69964f51ef875249aaf693ce87580
skipsuva <skip.suva@gmail.com>
2016-02-20 13:20:39 -0500

    add devise session filter, api working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,29 @@
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
   skip_before_action :authenticate!
+  respond_to :html, :json
 
+  # def create
+  #   binding.pry
+  #   user = User.authenticate(user_params)
+  #   if user
+  #     data = {
+  #       token: user.authentication_token,
+  #       email: user.email
+  #     }
+  #     render json: data, status: 201
+  #   end
+  # end
   def create
-    binding.pry
-    user = User.authenticate(user_params)
-    if user
-      data = {
-        token: user.authentication_token,
-        email: user.email
-      }
-      render json: data, status: 201
+    super do |user|
+      if request.format.json?
+        data = {
+          token: user.authentication_token,
+          email: user.email
+        }
+        render json: data, status: 201 and return
+      end
     end
   end
-
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:new, :edit]
   resources :pins, except: [:new, :edit]
