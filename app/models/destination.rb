73798ba7e045174bcc152b78196ace840d7a7aca
TimoSci <me@timosci.net>
2016-02-22 14:40:33 -0500

    add methods to change order of destination

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,6 +3,28 @@ class Destination < ActiveRecord::Base
   belongs_to :trip
   belongs_to :pin
 
+  #TODO worry about concurrency
+
+  def move_later
+    nekst = self.next
+    self.append_to(nekst) if nekst
+  end
+
+  def move_to_end
+    last = self.class.last_in_order
+    self.append_to(last) if self != last
+  end
+
+  def move_earlier
+    previous = self.previous
+    previous.append_to(self) if previous
+  end
+
+  def move_to_start
+    first = self.class.first_in_order
+    first.append_to(self) if self != first
+  end
+
   private
 
   def default_order
