fd458767f149d8443499867fbbe0fc9c67112e72
flats <dave@robador.com>
2016-02-22 17:30:06 -0500

    destinations create destroy

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -8,4 +8,17 @@ class TripsController < ApplicationController
   def show
     render json: Trip.find(params[:id])
   end
+
+  def create
+    trip = Trip.create(trip_params)
+    trip.user = authenticate!
+    trip.save
+    render json: trip
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :user_id, :user)
+  end
 end
