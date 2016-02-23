730196edbb729878f2a8cd9930816641178493e0
flats <dave@robador.com>
2016-02-23 17:20:08 -0500

    remove address from pin srslzr

--- app/serializers/pin_serializer.rb
+++ app/serializers/pin_serializer.rb
@@ -1,6 +1,6 @@
 class PinSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :nickname, :start_date, :description, :lat, :long, :visited, :address
+  attributes :id, :nickname, :start_date, :description, :lat, :long, :visited
 
   def lat
     object.place.lat
