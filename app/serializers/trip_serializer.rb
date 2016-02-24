ae5524539ddf6633da97807718ad316588bb537e
TimoSci <me@timosci.net>
2016-02-24 03:09:24 -0500

    serve hex color in api

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,9 @@
 class TripSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :name, :start_date, :user_id, :color
+  attributes :id, :name, :start_date, :user_id, :hex_color
+
+  def hex_color
+    object.hex_color
+  end
+  
 end
