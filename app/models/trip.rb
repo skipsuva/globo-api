814887ebefe31944d397bd44a0c15c7fa646d1f5
TimoSci <me@timosci.net>
2016-02-24 03:07:52 -0500

    create trip with default color

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,29 @@ class Trip < ActiveRecord::Base
   belongs_to :user
   has_many :destinations, dependent: :destroy
   has_many :pins, through: :destinations
+
+  before_create :set_color
+
+  def hex_color=(str)
+    str = str.gsub("#","")
+    str = str*2 if str.length == 3
+    self.color = str.to_i(16)
+  end
+
+  def hex_color
+    color.to_s(16).rjust(6,"0").prepend("#")
+  end
+
+  private
+
+  def set_color
+    #TODO select default color from palette
+    last_color = Trip.last.try(:color)
+    if not last_color
+      self.hex_color = "000"
+    else
+      self.color = (last_color + 32)%0xAAAAAA
+    end
+  end
+
 end
